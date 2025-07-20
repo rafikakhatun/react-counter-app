12c6569d12f0c77936712b13cc6fdf415d16bf8c
rafika khatun <rafikakhatun607@gmail.com>
2025-07-20 21:03:36 +0530

    Created Counter component with folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Counter from './component/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Counter></Counter>
      
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/component/Counter.jsx
@@ -0,0 +1,12 @@
+function Counter(){
+    return(
+        <>
+        
+        <h1>hello i am counter</h1>
+        
+        
+        </>
+    )
+}
+
+export default Counter;
