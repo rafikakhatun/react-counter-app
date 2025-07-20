16314bd85443a11b22d2f2a1904e88713c61145c
rafika khatun <rafikakhatun607@gmail.com>
2025-07-20 21:20:51 +0530

    Added counter logic to buttons

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -11,26 +11,17 @@ function Counter() {
             <div className="min-h-screen bg-gradient-to-br from-indigo-300 to-blue-400 flex items-center justify-center">
                 <div className="bg-white p-8 rounded-2xl shadow-xl text-center space-y-6 w-80">
                     <h1 className="text-2xl font-bold text-gray-700">React Counter</h1>
-                    <p className="text-5xl font-bold text-indigo-600"></p>
+                    <p className="text-5xl font-semibold text-indigo-600">{count}s</p>
 
 
                     <div className="flex justify-center gap-4">
-                        <button
-                            
-                            className="px-6 py-3 bg-green-500 hover:bg-green-600 text-white rounded-xl"
-                        >
+                        <button onClick={()=>setCount(count+1)} className="px-6 py-3 bg-green-500 hover:bg-green-600 text-white rounded-xl">
                             <Plus size={20} /> {/* + Icon */}
                         </button>
-                        <button
-                            
-                            className="px-6 py-3 bg-red-500 hover:bg-red-600 text-white rounded-xl"
-                        >
+                        <button onClick={()=>setCount(count-1)} className="px-6 py-3 bg-red-500 hover:bg-red-600 text-white rounded-xl">
                              <Minus size={20} /> {/* - Icon */}
                         </button>
-                        <button
-                            
-                            className="px-6 py-3 bg-gray-500 hover:bg-gray-600 text-white rounded-xl"
-                        >
+                        <button onClick={()=>setCount(0)} className="px-6 py-3 bg-gray-500 hover:bg-gray-600 text-white rounded-xl">
                              <RotateCcw size={20} /> {/* Reset Icon */}
                         </button>
                     </div>
