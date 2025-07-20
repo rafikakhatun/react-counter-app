9dccd6ed7447edcab7926d5469885ace920b4947
rafika khatun <rafikakhatun607@gmail.com>
2025-07-20 21:06:47 +0530

    Designed Counter UI using Tailwind CSS

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -1,10 +1,18 @@
-function Counter(){
-    return(
+function Counter() {
+    return (
         <>
+
+            
+            <div className="min-h-screen bg-gradient-to-br from-indigo-300 to-blue-400 flex items-center justify-center">
+                <div className="bg-white p-8 rounded-2xl shadow-xl text-center space-y-6 w-80">
+                    <h1 className="text-2xl font-bold text-gray-700">React Counter</h1>
+                    <p className="text-5xl font-bold text-indigo-600">10s</p>
+                </div>
+            </div>
         
-        <h1>hello i am counter</h1>
-        
-        
+
+
+
         </>
     )
 }
